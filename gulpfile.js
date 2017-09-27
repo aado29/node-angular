be12f294541ea21ad9f95128977b72225296c675
Alberto Diaz <aado29@gmail.com>
2017-09-27 10:26:27 -0400

    added gulp bundler to the project

--- gulpfile.js
+++ gulpfile.js
@@ -7,17 +7,24 @@ var sourcemaps = require('gulp-sourcemaps')
 gulp.task('scripts', function () {
   gulp.src(['./app_client/**/*.js', '!./app_client/**/*.test.js', '!./app_client/app.min.js'])
 		.pipe(sourcemaps.init())
-			.pipe(concat('./app.min.js'))
-			.pipe(uglify({mangle: true}))
-			.pipe(gulp.dest('app_client'))
+		.pipe(concat('./app.min.js'))
+		.pipe(uglify({mangle: true}))
 		.pipe(sourcemaps.write('./'))
-		.pipe(gulp.dest('app_client'))
+		.pipe(gulp.dest('public/js/'))
+})
+
+gulp.task('ng-templates', function () {
+	gulp.src(['./app_client/**/*.html'])
+		.pipe(gulp.dest('public/'))
 })
 
 gulp.task('watch', () => {
   watch(['./app_client/**/*.js', '!./app_client/**/*.test.js', '!./app_client/app.min.js'], function () {
     gulp.start('scripts')
   })
+  watch(['./app_client/**/*.html'], function () {
+  	gulp.start('ng-templates')
+  })
 })
 
-gulp.task('default', ['scripts', 'watch'])
+gulp.task('default', ['ng-templates', 'scripts', 'watch'])
